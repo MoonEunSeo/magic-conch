1f8892d1580a900b00b21ef9bc63dc13f61a0a00
YellowTV <mos8594@naver.com>
2025-10-30 03:05:35 +0900

    seo까지 3

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
+  root: ".", // 현재 frontend가 루트
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    rollupOptions: {
+      input: resolve(__dirname, "index.html"),
+    },
+  },
 });

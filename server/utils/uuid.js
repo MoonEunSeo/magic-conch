a1c0262b10c562dee272d44946e3b772330a1f34
YellowTV <mos8594@naver.com>
2025-11-06 02:08:41 +0900

    uuid 왜못받아오는데

--- server/utils/uuid.js
+++ server/utils/uuid.js
@@ -2,9 +2,15 @@
 export function getOrCreateUserUUID() {
     let uuid = localStorage.getItem("magic_conch_uuid");
     if (!uuid) {
-      uuid = crypto.randomUUID();
+      if (window.crypto?.randomUUID) {
+        uuid = crypto.randomUUID();
+      } else {
+        // Safari fallback
+        uuid = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+          (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+      }
       localStorage.setItem("magic_conch_uuid", uuid);
     }
     return uuid;
   }
-  
